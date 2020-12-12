bc480683e8309ab41da27db5a933da1f61dd5c27
Ahad Chowdhury <ahad@maacpiash.com>
2020-12-12 20:12:13 +1100

    Add @BlazorFluentUI dependency and modifications

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BlazorFluentUI;
 using FabricUI.Data;
 
 namespace FabricUI
@@ -29,6 +30,7 @@ namespace FabricUI
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazorFluentUI();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
